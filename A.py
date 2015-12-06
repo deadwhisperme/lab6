3cc5cea716e9b79c3bb25bea0d6144e6f1da6afb
deadwhisperme <ekaterinacydenzhapova@mail.ru>
2015-12-06 18:02:07 +0300

    Update A.py

--- A.py
+++ A.py
@@ -16,3 +16,18 @@
 | 8 3 5 4 5 1 |       |
 +-------------+-------+
 '''
+inp = open("input.txt","r") 
+otp = open("output.txt","w") 
+N = list(inp.readlines()) 
+A = list(map(int,N[1].split(" "))) 
+for i in range(int(N[0])): 
+    if A.count(A[i]) == 2: 
+        otp.write(str(A[i])) 
+        otp.close() 
+        inp.close() 
+        break 
+    if A.count(A[int(N[0])-1-i]) == 2: 
+        otp.write(str(A[int(N[0])-1-i])) 
+        otp.close() 
+        inp.close() 
+        break 
