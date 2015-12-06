ba1c02615cc2e0ed9ae10d23ba8bb469a156a179
deadwhisperme <ekaterinacydenzhapova@mail.ru>
2015-12-06 18:03:50 +0300

    Update C.py

--- C.py
+++ C.py
@@ -25,3 +25,21 @@
   | -40 41 -42 -41 42 40 |       |
   +----------------------+-------+
 '''
+q = 0
+inp = open('input.txt','r')
+out = open('output.txt','w')
+N = int(inp.readline())
+A = inp.readline().split()
+for i in range(N):
+  A[i] = int(A[i])
+m = N + 1
+for i in range(N):
+  if((A[i]) < 0) and (A[i+1:].count(-A[i]) > 0):
+    q = A.index(-A[i])-i
+    if q < m:
+      m = q
+if m == N + 1:
+  print(0, file=out)
+else:
+  print(m, file=out)
+
